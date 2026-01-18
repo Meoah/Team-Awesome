bdeba24f901f5f6c32183febeee8a804fcc2e43e
TCalvin101 <tcalvin@usv.edu>
2026-01-18 09:26:05 -0800

    Main Menu updates
    All main menu buttons are functional. The settings features are functional

--- MainMenu/main_menu.gd
+++ MainMenu/main_menu.gd
@@ -1,10 +1,12 @@
 extends Control
+@onready var main_buttons = $MainButtons
+@onready var settings = $Settings
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	main_buttons.visible = true
+	settings.visible = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -17,8 +19,14 @@ func _on_start_pressed():
 
 func _on_settings_pressed():
 	print("Settings Pressed")
-
+	main_buttons.visible = false
+	settings.visible = true
+	
 
 func _on_exit_pressed():
 	get_tree().quit() #exits out of game
 	
+
+
+func _on_backbutton_pressed():
+	_ready()
