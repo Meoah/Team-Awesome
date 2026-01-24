68c150429300fad377229c4e89a8c5fdb1cc7494
DarvenAcevedo <darven.acevedo2021@gmail.com>
2026-01-23 17:08:52 -0800

    Added System for Checking inputs
    Added system that checks what your current input is and if it match the first item of the input_array it gives a positive response, if not it gives a negative response. If array is empty then prints completion text

--- Scenes/arrows.gd
+++ Scenes/arrows.gd
@@ -3,9 +3,15 @@ extends Node2D
 @export var input_length : int = 3
 
 var input_array : Array[String]
-var potential_inputs : Array[String] = ["left","right","up","down"]
+var potential_inputs : Array[String] = ["Left","Right","Up","Down"]
+var current_input = "0"
+var correct_input = "0"
+
+
 func _ready() -> void:
 	print(generate_inputs())
+	print(correct_input)
+	
 
 
 func generate_inputs()-> Array[String]:
@@ -13,4 +19,25 @@ func generate_inputs()-> Array[String]:
 	for i in input_length:
 		var input = potential_inputs.pick_random()
 		return_array.append(input) 
+	input_array = return_array
+	print(input_array)
 	return return_array
+	
+
+func _input(event : InputEvent):
+	if not input_array.is_empty():
+		if Input.is_action_just_pressed("Up") or Input.is_action_just_pressed("Down") or Input.is_action_just_pressed("Right") or Input.is_action_just_pressed("Left") :
+			current_input = event.as_text()
+			if current_input == input_array[0]:
+				print("cool")
+				input_array.remove_at(0)
+				print(input_array)
+			else:
+				print("Lame!")
+			clear()
+
+func clear():
+	if input_array.size() == 0:
+		print("You Win!!")
+		
+		
