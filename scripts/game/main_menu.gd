6602f9b30d34fb7352e9767b61b281573bfda300
Meoah <78709303+Meoah@users.noreply.github.com>
2026-01-28 09:02:26 -0800

    Revert "Merge pull request #14 from Meoah/Teresa"
    This reverts commit 4fe2406ceabf26e5ab09df435d24dc2b915437c8, reversing
    changes made to 3d25c3f33ffadd0fa71cf3239e9b8419f926652b.

--- scripts/game/main_menu.gd
+++ scripts/game/main_menu.gd
@@ -4,32 +4,20 @@ extends Control
 @onready var settings = $Settings
 @onready var gameStart
 
-
+@export_file  ("*.tscn") var start_scene_path: String 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	main_buttons.visible = true
 	settings.visible = false
-<<<<<<< Updated upstream:scripts/game/main_menu.gd
-<<<<<<< Updated upstream:scripts/game/main_menu.gd
-<<<<<<< Updated upstream:scripts/game/main_menu.gd
-<<<<<<< Updated upstream:scripts/game/main_menu.gd
 	$AudioStreamPlayer.play(65)
-=======
-=======
->>>>>>> Stashed changes:MainMenu/main_menu.gd
-=======
->>>>>>> Stashed changes:MainMenu/main_menu.gd
-=======
->>>>>>> Stashed changes:MainMenu/main_menu.gd
-
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	pass
->>>>>>> Stashed changes:MainMenu/main_menu.gd
 
 
 func _on_start_pressed():
-	get_tree().change_scene_to_file("res://vro.tscn") #starts game
+	get_tree().change_scene_to_file(start_scene_path) #starts game
 
 
 func _on_settings_pressed():
