5ec1a8e2f28901985c8012603e20f4cde39787ef
TCalvin101 <tcalvin@usv.edu>
2026-02-04 10:25:16 -0800

    Changes to title screen
    Change to title screen
    moved some files into another folder under UI

--- scripts/game/inventory_shop.gd
+++ scripts/game/inventory_shop.gd
@@ -10,11 +10,27 @@ var FISH04 = preload("res://assets/textures/fish/doodle_fish/Fish04.tres")
 
 var ALL_FISH := [FISH01, FISH02, FISH03, FISH04]
 
+#Finds FishGrid Container
+@onready var fish_grid = $FishIconPanel/FishFrid
+
 func _ready() -> void:
 	#Show what fish the player has and the amount when shop opens
 	for fish in ALL_FISH:
 		var count := Inventory.get_fish_count(fish)
 		print("Player has", count, "x", fish.display_name, "(price: )", fish.fish_price, ")")
+
+#Shows icons inside of the shop UI
+func _show_fish_icons() -> void:
+	#clear out any icons
+	for child in fish_grid.get_children():
+		child.queue_free()
+	#Create an icon and add it to the grid
+	for fish in ALL_FISH:
+		if fish.icon == null:
+			continue
+		var icon_rect := TextureRect.new()
+		icon_rect.texture = fish.icon
+	
 
 func sell_fish(fish: FishData) -> void:
 	if not Inventory.has_fish(fish):
