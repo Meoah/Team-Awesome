3dbd5df43f708937b9be797fe90bd876e5cab409
TCalvin101 <tcalvin@usv.edu>
2026-02-04 08:07:23 -0800

    Changed Inventory.gd
    changed Inventory.gd to match FishData.gd resource file

--- scripts/system/player_data.gd
+++ scripts/system/player_data.gd
@@ -2,6 +2,7 @@ extends Node
 class_name PlayerData
 
 var playerScore : float = 0.0
+var inventory : Array = []
 
 func set_score(score : float) -> void:
 	playerScore = score
--- scripts/ui/inventory_ui.gd
+++ scripts/ui/inventory_ui.gd
@@ -3,6 +3,9 @@ extends Control
 #Finds the grid container with the slots
 @onready var slot_grid = $Panel/SlotGrid
 
+#Retrieves a list of the fish types the player has
+var fish_list = Inventory.get_fish_list()
+
 func _ready():
 	update_inventory_ui()
 	
@@ -15,9 +18,10 @@ func update_inventory_ui() -> void:
 		var slot = slots[i]
 		var label = slot.get_node("Label")
 		
-		if i < Inventory.inventory.size():
-			var fish_id = Inventory.inventory[i]
-			var fish_name = Inventory.get_fish_name(fish_id)
+		if i < fish_list.size():
+			var fish = fish_list[i]
+			var fish_name = fish.fish_name
+			var amount = Inventory.get_fish_count(fish)
 			label.text = fish_name
 		else:
 			label.text = ""
