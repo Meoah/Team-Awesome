bfcab3dec97fc73cfef848dd3159cbbba3e40463
DarvenAcevedo <darven.acevedo2021@gmail.com>
2026-02-23 15:21:03 -0800

    Added failstate items for each category

--- scripts/util/upgrades_data.gd
+++ scripts/util/upgrades_data.gd
@@ -4,19 +4,23 @@ class_name UpgradesData
 # Table of Contents. Each upgrade is split into a category. Refer to category and item ID to access data.
 
 const UPGRADES_EXOTIC : Dictionary = {
+	0 : DUMMY_EXOTIC,
 	1 : MULTI_BOBBER,
 }
 
 const UPGRADES_REEL : Dictionary = {
+	0 : DUMMY_REEL,
 	1 : WOODEN_REEL,
 	2 : FISHTAGRAM_REEL,
 }
 
 const UPGRADES_RODS : Dictionary = {
+	0 : DUMMY_ROD,
 	1 : WOODEN_ROD,
 }
 
 const UPGRADES_LURES : Dictionary = {
+	0 : DUMMY_LURE,
 	1 : PLASTIC_LURE,
 	
 }
@@ -34,6 +38,13 @@ const UPGRADES_LURES : Dictionary = {
 
 
 #Reels
+const DUMMY_REEL : Dictionary = {
+	"name" : "Dummy Reel",
+	"image" : NodePath("res://assets/textures/upgrades/reels/Wooden_Reel_temp.png"),
+	"type" : "Reel",
+	"cost" : 0.0,
+	"description" : "You shouldnt see this... tsk tsk tsk"
+}
 
 const WOODEN_REEL : Dictionary = {
 	"name" : "Wooden Reel",
@@ -55,15 +66,32 @@ const FISHTAGRAM_REEL : Dictionary = {
 
 #Rods
 
+const DUMMY_ROD : Dictionary = {
+	"name" : "Dummy Rod",
+	"image" : NodePath("res://assets/textures/upgrades/rods/wooden_rod_temp.png"),
+	"type" : "Reel",
+	"cost" : 0.0,
+	"description" : "Yo rod is so STUPID... they didnt even know this was an ERROR message!"
+}
+
 const WOODEN_ROD : Dictionary = {
-	"name" : "Wooden Reel",
+	"name" : "Wooden Rod",
 	"image" : NodePath("res://assets/textures/upgrades/rods/wooden_rod_temp.png"),
 	"type" : "Reel",
 	"cost" : 15.0,
 	"description" : "Adds +2% value to every other fish caught!"
 }
 
+
+
 #Lures
+const DUMMY_LURE : Dictionary = {
+	"name" : "Dummy Lure",
+	"image" : NodePath("res://assets/textures/upgrades/lures/plastic_lure_temp.png"),
+	"type" : "Lure",
+	"cost" : 0.0,
+	"description" : "You just got lured DUMMY!"
+}
 
 const PLASTIC_LURE : Dictionary = {
 	"name" : "Plastic Lure",
@@ -74,6 +102,14 @@ const PLASTIC_LURE : Dictionary = {
 }
 
 #Exotics
+const DUMMY_EXOTIC : Dictionary = {
+	"name" : "Dummy exotic",
+	"image" : NodePath("res://assets/textures/upgrades/exotic/multi_bobber.png"),
+	"type" : "Exotic",
+	"cost" : 0.0,
+	"description" : "Does nothing... exotically~"
+}
+
 const MULTI_BOBBER : Dictionary = {
 	"name" : "Multi Bobber",
 	"image" : NodePath("res://assets/textures/upgrades/exotic/multi_bobber.png"),
