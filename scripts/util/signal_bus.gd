904446d652978080c52f691e7b759a3749244551
DarvenAcevedo <darven.acevedo2021@gmail.com>
2026-03-04 20:46:45 -0800

    Tried to piece it all back together
    Got some of my work recovered. Some of Li's HUD stuff is missing so that'll need to be fixed.

--- scripts/util/signal_bus.gd
+++ scripts/util/signal_bus.gd
@@ -9,13 +9,12 @@ signal card_purchased(purchased_card_info : Dictionary)
 signal shop_closed(save_data : Array[Dictionary], shop_type : ShopPopup.SHOP_TYPE_FLAGS)
 
 
-<<<<<<< HEAD
+
 # HUD Signal
 
 signal slot_left_clicked
 
-=======
->>>>>>> 69fa1fb8799a8313aef9dcb175195a758598b0b8
+
 
 # Intro Signals
 signal run_intro
